bea242fdb13caa0375368a7eb49446ed9f811b20
chadyoungdell.com <chad.young@dell.com>
2017-09-14 13:07:56 -0500

    initial commit, pythong function not working
    Signed-off-by: chadyoungdell.com <chad.young@dell.com>

---
+++ src/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import message
+
+# This is a simple "Hello World" python app
+
+print("Hello, World!")
+
+# Call the punction from message.py file
+message.funcMessage()
+
---
+++ src/message.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    print("This is from the message.py file")
+
+
+def funcMessage():
+    print("This is from the funcMessage function in the message.py file")
+
+
+# Call the main funtion
+main()
